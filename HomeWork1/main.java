263799f87293554de5b17febbad3309b88905568
gordiyukolesya <47782707+gordiyukolesya@users.noreply.github.com>
2019-02-19 15:22:33 +0200

    Add files via upload

---
+++ HomeWork1/main.java
@@ -0,0 +1,16 @@
+//import java.io.IOException;
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		int a=0;
+		System.out.print ("Enter desired number");
+	    a = scanner.nextInt();
+	    int b= a*a;
+	    System.out.print ("a^2 = ");
+	    System.out.println (b);
+	}
+	
+}
